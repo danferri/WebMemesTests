324e2782a38c8ac16ee24c68272cac62726c1bc0
Daniel Ferri <89649179+danferri@users.noreply.github.com>
2024-11-26 19:58:50 -0300

    feat: initial implementation of CadastroMemePage page object.

--- src/main/java/br/edu/ifsp/Main.java
+++ src/main/java/br/edu/ifsp/Main.java
@@ -2,6 +2,6 @@ package br.edu.ifsp;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/test/java/page/CadastroMemePage.java
@@ -0,0 +1,39 @@
+package page;
+
+import model.Meme;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CadastroMemePage {
+    protected final WebDriver driver;
+
+    private final By tipoMemeSelect = By.xpath("//*[@id=\"type\"]");
+    private final By urlMemeInput = By.xpath("//*[@id=\"url\"]");
+    private final By tituloMemeInput = By.xpath("//*[@id=\"title\"]");
+    private final By descricaoMemeInput = By.xpath("//*[@id=\"comment\"]");
+    private final By cadastrarMemeButton = By.xpath("//*[@id=\"memeForm\"]/button");
+    private final By visualizarMemesLink = By.xpath("/html/body/div[1]/a");
+
+
+
+    public CadastroMemePage(WebDriver driver) {
+        this.driver = driver;
+
+        if(!driver.getCurrentUrl().equals("https://webmemes.devhub.dev.br/index.html")) {
+            throw new IllegalStateException("This is not the Cadastro de Meme page," +
+                    "current page is: " + driver.getCurrentUrl());
+        }
+    }
+
+    public void goToMemesCadastradosPage() {
+        driver.findElement(visualizarMemesLink).click();
+    }
+
+    public void cadastroMemeFromMeme(Meme meme) {
+        Select selectTipo = new Select(driver.findElement(tipoMemeSelect));
+        selectTipo.selectByValue(meme.getTipo().getString());
+
+    }
+
+}
