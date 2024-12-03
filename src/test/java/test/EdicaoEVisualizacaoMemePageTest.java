759e6b05b8c5785023db3238bfd65dc45fb52d1e
Luigi <a.luigi@aluno.ifsp.edu.br>
2024-12-02 22:39:02 -0300

    test:Should not stay the message of edit with success after delete it

--- src/test/java/test/EdicaoEVisualizacaoMemePageTest.java
+++ src/test/java/test/EdicaoEVisualizacaoMemePageTest.java
@@ -189,6 +189,48 @@ public class EdicaoEVisualizacaoMemePageTest {
             assertEquals("https://www.hondacaiuas.com.br/wp-content/uploads/2022/08/tipos-de-carro-hatch-new-city-hatchback.jpg",imagemUrl);
         }
 
+        @Test
+        @DisplayName("Should not stay the message of edit with success after delete it")
+        void ShouldNotStayTheMessageOfEditWithSuccessAfterDeleteIt(){
+            updateAndViewMeme.goToRegistrationPage();
+            assertEquals("https://webmemes.devhub.dev.br/index.html", driver.getCurrentUrl());
+
+            WebElement selectElement = driver.findElement(By.id("type"));
+            Select select = new Select(selectElement);
+            select.selectByIndex(0);//seleciona imagem
+
+
+            driver.findElement(By.id("url")).sendKeys("https://www.hondacaiuas.com.br/wp-content/uploads/2022/08/tipos-de-carro-hatch-new-city-hatchback.jpg");
+            driver.findElement(By.id("title")).sendKeys("New Meme");
+            driver.findElement(By.id("comment")).sendKeys("This is a test meme");
+
+            driver.findElement(By.xpath("//button[text()='Cadastrar Meme']")).click();
+
+            driver.get("https://webmemes.devhub.dev.br/visualizar.html");
+
+            updateAndViewMeme.editButton();
+
+            WebElement titleField = driver.findElement(By.id("title"));
+            titleField.clear();
+            titleField.sendKeys("Edit Title");
+
+            driver.findElement(By.id("submitMeme")).click();
+
+            updateAndViewMeme.removeButton();
+
+            try {
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\"message\"]")));
+
+                assertTrue(true);
+            } catch (TimeoutException e) {
+                fail("A mensagem de sucesso ainda está visível após a remoção.");
+            }
+
+
+
+        }
+
 
     }
 
